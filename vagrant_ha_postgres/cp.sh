0eedec0ab21efbe7aca3eca20d936539f88a41d0
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-02-26 21:17:58 +0200

    add cp.sh

---
+++ vagrant_ha_postgres/cp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+target=$1
+case $target in
+  master)
+    cp ./postgresql.conf.master /etc/postgresql/12/main/postgresql.conf
+    cp ./pg_hba.conf.master /etc/postgresql/12/main/pg_hba.conf
+    ;;
+  slave)
+    cp ./postgresql.conf.slave /etc/postgresql/12/main/postgresql.conf
+    ;;
+  replicator)
+    psql -c "CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator';"
+    ;;
+  slot)
+    psql -c "select * from pg_create_physical_replication_slot('slave1_slot');"
+    ;;
+  backup)
+    pg_basebackup --pgdata /var/lib/postgresql/data --format=p \
+        --write-recovery-conf --checkpoint=fast --label=mffb --progress \
+        --host=pgmaster --port=5432 --username=replicator
+    ;;
+  *)
+    echo 'Invalid target'
+    ;;
